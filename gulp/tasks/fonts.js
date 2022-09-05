5d962a34d7b8769dd681a28d4bcd566a3d74446f
mshornikov <mikhail.shornikov@mail.ru>
2022-09-05 14:54:09 +0300

    Add fontsStyle function

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,6 +1,7 @@
 import fs, { appendFile } from 'fs';
 import fonter from 'gulp-fonter';
 import ttf2woff2 from 'gulp-ttf2woff2';
+import { url } from 'inspector';
 
 export const otfToTtf = () => {
     // Search for font files .otf
@@ -63,10 +64,39 @@ export const fontsStyle = () => {
                             fontWeight = 100;
                         } else if (fontWeight.toLowerCase() === 'extralight') {
                             fontWeight = 200;
+                        } else if (fontWeight.toLowerCase() === 'light') {
+                            fontWeight = 300;
+                        } else if (fontWeight.toLowerCase() === 'medium') {
+                            fontWeight = 500;
+                        } else if (fontWeight.toLowerCase() === 'semibold') {
+                            fontWeight = 600;
+                        } else if (fontWeight.toLowerCase() === 'bold') {
+                            fontWeight = 700;
+                        } else if (fontWeight.toLowerCase() === 'extrabold' || fontWeight.toLowerCase() === 'heavy') {
+                            fontWeight = 800;
+                        } else if (fontWeight.toLowerCase() === 'black') {
+                            fontWeight = 900;
+                        } else {
+                            fontWeight = 400;
                         }
+                        fs.appendFile(fontsFile, 
+                            // `@font-face {
+                            //     font-family: ${fontName};
+                            //     font-display: swap;
+                            //     src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff format("woff");)
+                            //     font-weight: ${fontWeight};
+                            //     font-style: normal;
+                            // }\r\n`, cb);
+                        `@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
+                        newFileOnly = fontFileName;
                     }
                 }
+            } else {
+                console.log("File scss/fonts.scss already exists. For file update you should delete it");
             }
         }
-    })
+    });
+
+    return app.gulp.src(`${app.path.srcFolder}`);
+    function cb() {}
 }
